f5e22a716d051e8ac0f51740289bb5c21e723963
Michael Carbone <carbonmk@bc.edu>
2022-08-31 16:51:42 -0400

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Michael Carbone on 8/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Michael Carbone on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
