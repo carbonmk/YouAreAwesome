dcb1f72882c65882d4dca1707c3f3acf24e0e7d6
Michael Carbone <carbonmk@bc.edu>
2022-09-05 15:42:48 -0400

    Chap 1.5

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = " "
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .padding()
+                .frame(height: 150)
+                .frame( maxWidth: .infinity)
+                .border(.orange, width: 1)
+            
+            HStack{
+                Button("Awesome") {
+                    //action when but is pressed
+                    messageString = "You are awesome"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    messageString = "You are great"
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .border(.purple, width:5)
         }
-        .padding()
     }
 }
 
