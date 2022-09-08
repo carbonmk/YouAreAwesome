dd9eb7324834ac028b61ca56d51c20015f4a75fe
Michael Carbone <carbonmk@bc.edu>
2022-09-08 13:17:40 -0400

    Chap 1.8

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = " "
+    @State private var messageString = "Namaste"
     
     var body: some View {
         VStack {
             Spacer()
+            Image(systemName: "speaker.wave.2", variableValue: 0.5)
+                .resizable()
+                .scaledToFit()
+                .symbolRenderingMode(.multicolor)
+                .padding()
+                .background(Color(hue: 0.523, saturation: 0.342, brightness: 1.0))
+                .cornerRadius(30)
+                .shadow(color: .gray, radius:30, x: 20, y: 20)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(.teal, lineWidth: 1))
+                .padding()
+                
+            
             
             Text(messageString)
                 .font(.largeTitle)
@@ -23,9 +37,9 @@ struct ContentView: View {
                 .padding()
                 .frame(height: 150)
                 .frame( maxWidth: .infinity)
-            //    .border(.orange, width: 1)
+//                .border(.orange, width: 1)
                 .padding()
-            
+
             Spacer()
             
             
