e15dc66956765e4a2f4ea28a9ff1d4141e8e418d
Michael Carbone <carbonmk@bc.edu>
2022-09-10 15:30:42 -0400

    Chap 1.11

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var messageString = "Namaste"
+    @State private var imageName = ""
+    
     
     var body: some View {
         VStack {
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+                .padding()
             Spacer()
             
             Text(messageString)
@@ -33,8 +41,12 @@ struct ContentView: View {
             Button("Show Message") {
                 let message1 = "You are awesome"
                 let message2 = "You are great"
+                
+                let im0 = "image0"
+                let im1 = "image1"
                 //action when but is pressed
                 messageString = (messageString == message1 ? message2 : message1)
+                imageName = (imageName == im0 ? im1 : im0)
             }
                     .buttonStyle(.borderedProminent)
                 
